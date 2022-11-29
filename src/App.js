1f5228a75289cf7892c551a7a668e6ed11576f54
Hoda Omar <hodaomer@gmail.com>
2022-11-29 00:49:05 -0500

    form project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
-import {useState, useEffect} from 'react';
-import './App.css';
+import "./App.css";
+import { useState, useRef } from 'react'
+import React from "react";
 
-const App=() => {
-  const [counter, setCounter] = useState(0);
-  
-  useEffect(() => {
-    alert("You've changed the counter to" + counter);
-  },[counter]);
+function App() {
 
-  
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+
+  const emailRef = useRef();
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+
+
+
   return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+    <div class="App">
+    <header class="Header">
+      <h1> Enter Info Below:</h1>
+      <label htmlFor="email">Email Address</label>
+      <br />
+      <input type="text" name="email" ref={emailRef} />
+      <br />
 
+      <label htmlFor="first-name">First Name : </label>
+      <br />
+      <input type="text" name="first-name" ref={firstNameRef} />
+      <br />
+
+      <label htmlFor="last-name"> Last Name : </label>
+      <br />
+      <input type="text" name="last-name" ref={lastNameRef} />
+
+      <br />
+      <br />
+
+      <button class="btn"
+        onClick={function () {
+          setEmail(emailRef.current.value);
+          setFirstName(firstNameRef.current.value);
+          setLastName(lastNameRef.current.value);
+        }}
+      >
+        Submit 
+      </button>
+    </header>
+      <br />
+      <br />
+
+      <div class="items">
+        Email Adress: {email}
+        <br />
+        First Name : {firstName}
+        <br />
+        Last Name : {lastName}
+      </div>
+  
     </div>
   );
 }
